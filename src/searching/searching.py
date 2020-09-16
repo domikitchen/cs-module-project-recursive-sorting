8d8873dc4651ada8543b193b1f02e2809b0d3f73
Dominique K Kitchenakow <domikitchen@users.noreply.github.com>
2020-09-15 19:05:49 -0500

    completed mvp

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,7 +1,17 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
-    # Your code here
-
+    if len(arr) > 0:
+        midpoint = (end + start) // 2
+        if arr[midpoint] == target:
+            return midpoint
+        elif arr[midpoint] < target:
+            return binary_search(arr, target, end, (midpoint + 1))
+        elif arr[midpoint] > target:
+            return binary_search(arr, target, (midpoint - 1), start)
+        else:
+            return -1
+    else:
+        return -1
 
 # STRETCH: implement an order-agnostic binary search
 # This version of binary search should correctly find 
@@ -11,4 +21,5 @@ def binary_search(arr, target, start, end):
 # or iteratively
 def agnostic_binary_search(arr, target):
     # Your code here
+    pass
 
--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -1,19 +1,45 @@
 # TO-DO: complete the helper function below to merge 2 sorted arrays
 def merge(arrA, arrB):
-    elements = len(arrA) + len(arrB)
-    merged_arr = [0] * elements
 
     # Your code here
+    merged_arr = []
+    
+    s = 0
+    e = 0
 
+    while s < len(arrA) and e < len(arrB):
+        print(f"arrA: {arrA}  arrB: {arrB}")
+        if arrA[s] < arrB[e]:
+            print(f"  arrA: {arrA}")
+            # merged_arr.pop(0)
+            merged_arr.append(arrA[s])
+            s += 1
+        else:
+            print(f"  arrB: {arrB}")
+            # merged_arr.pop(0)
+            merged_arr.append(arrB[e])
+            e += 1
 
+    merged_arr += arrA[s:]
+    merged_arr += arrB[e:]
+
+    print(f"merged boi:{merged_arr}\n")
     return merged_arr
 
 # TO-DO: implement the Merge Sort function below recursively
+
 def merge_sort(arr):
-    # Your code here
+    if len(arr) <= 1:
+        return arr
+    
+    middy = len(arr) // 2
+    start = merge_sort(arr[:middy])
+    end = merge_sort(arr[middy:])
 
+    return merge(start, end)
 
-    return arr
+arrsfd = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
+print(merge_sort(arrsfd))
 
 # STRETCH: implement the recursive logic for merge sort in a way that doesn't 
 # utilize any extra memory
@@ -21,8 +47,9 @@ def merge_sort(arr):
 # or data structures; it can only re-use the memory it was given as input
 def merge_in_place(arr, start, mid, end):
     # Your code here
+    pass
 
 
 def merge_sort_in_place(arr, l, r):
     # Your code here
-
+    pass
